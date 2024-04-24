2e31d9d4eda327836098c9592a764785e66ce17c
Raddames Tonui <raddamestonui48@gmail.com>
2024-04-24 19:25:49 +0300

    Update BlogPost.js

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -3,12 +3,13 @@ import BlogContent from "./BlogContent";
 import Comment from "./Comment";
 
 function BlogPost() {
+ 
   return (
     <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <BlogContent isPublished={true} articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." minutesToRead= {77}/>  
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
-      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451"/>
     </div>
   );
 }
